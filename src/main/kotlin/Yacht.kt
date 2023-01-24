ad3fe6c79536ada6993986a600cf3d14770ac927
Nathan Cullen <Nathan.Cullen@autotrader.co.uk>
2023-01-24 14:10:29 +0000

    Passed tests for single number categories

--- src/main/kotlin/Yacht.kt
+++ src/main/kotlin/Yacht.kt
@@ -3,12 +3,12 @@ object Yacht {
     fun solve(category: YachtCategory, vararg dices: Int): Int {
         return when (category) {
             YachtCategory.YACHT -> if (dices.toSet().size == 1) 50 else 0
-            YachtCategory.ONES -> 0
-            YachtCategory.TWOS -> 0
-            YachtCategory.THREES -> 0
-            YachtCategory.FOURS -> 0
-            YachtCategory.FIVES -> 0
-            YachtCategory.SIXES -> 0
+            YachtCategory.ONES -> sumDiceOfValue(1, dices)
+            YachtCategory.TWOS -> sumDiceOfValue(2, dices)
+            YachtCategory.THREES -> sumDiceOfValue(3, dices)
+            YachtCategory.FOURS -> sumDiceOfValue(4, dices)
+            YachtCategory.FIVES -> sumDiceOfValue(5, dices)
+            YachtCategory.SIXES -> sumDiceOfValue(6, dices)
             YachtCategory.FULL_HOUSE -> 0
             YachtCategory.FOUR_OF_A_KIND -> 0
             YachtCategory.LITTLE_STRAIGHT -> 0
@@ -16,4 +16,8 @@ object Yacht {
             YachtCategory.CHOICE -> 0
         }
     }
+}
+
+private fun sumDiceOfValue(value: Int, dices: IntArray): Int {
+    return dices.filter { it == value }.sum()
 }
--- src/test/kotlin/YachtTest.kt
+++ src/test/kotlin/YachtTest.kt
@@ -5,9 +5,30 @@ import YachtCategory.*
 class YachtTest {
 
     @Test
-    fun shouldReturn50ForYacht() = assertEquals(50, Yacht.solve(YACHT, 5, 5, 5, 5, 5))
+    fun yacht() = assertEquals(50, Yacht.solve(YACHT, 5, 5, 5, 5, 5))
 
     @Test
-    fun `shouldReturn0ForNotYacht`() = assertEquals(0, Yacht.solve(YACHT, 1, 3, 3, 2, 5))
+    fun notYacht() = assertEquals(0, Yacht.solve(YACHT, 1, 3, 3, 2, 5))
 
+    @Test
+    fun ones() = assertEquals(3, Yacht.solve(ONES, 1, 1, 1, 3, 5))
+
+    @Test
+    fun twos() = assertEquals(2, Yacht.solve(TWOS, 2, 3, 4, 5, 6))
+
+    @Test
+    fun threes() = assertEquals(3, Yacht.solve(THREES, 3, 1, 1, 5, 1))
+
+    @Test
+    fun fours() = assertEquals(8, Yacht.solve(FOURS, 1, 4, 1, 4, 1))
+
+    @Test
+    fun fives() = assertEquals(15, Yacht.solve(FIVES, 1, 5, 1, 5, 5))
+
+    @Test
+    fun sixes() = assertEquals(18, Yacht.solve(SIXES, 1, 6, 6, 6, 1))
+
+
+    @Test
+    fun notOnes() = assertEquals(0, Yacht.solve(ONES, 4, 3, 6, 5, 5))
 }
